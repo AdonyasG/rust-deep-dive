ca2270251c5c6b7eea83700c0bff6727babfcc8f
Adonyas Getachew <adonyas2000@gmail.com>
2026-04-24 18:35:44 +0300

    Update base path in Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // GH Pages serves at https://<user>.github.io/<repo>/
 // For user/org sites (<user>.github.io repo) or custom domains, use base: '/'
 export default defineConfig({
-  base: '/',      // ← change to your repo name
+  base: '/rust-deep-dive/',      // ← change to your repo name
   plugins: [react(), tailwindcss()],
 })
